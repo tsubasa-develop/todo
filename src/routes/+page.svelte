47fb0698caba26ee105eda0416d39058f3e76e0e
tsubasa_develop <t.ohtsuka@re-tech.net>
2023-07-27 17:10:06 +0900

    update: routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,63 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	type Todo = {
+		id: number;
+		task: string;
+	};
+
+	let todos: Todo[] = [];
+	let task = '';
+
+	const addTask = () => {
+		if (task === '') return;
+		todos = [...todos, { id: todos.length + 1, task: task }];
+		saveTodos();
+	};
+
+	const deleteTask = (id: number) => {
+		todos = todos.filter((todo) => todo.id !== id);
+		saveTodos();
+	};
+
+	// ローカルストレージからTODOSを取得
+	const getTodos = () => {
+		if (!browser) return [];
+		const todos = localStorage.getItem('todos');
+		if (!todos) return [];
+		return JSON.parse(todos);
+	};
+
+	// ローカルストレージにTODOSを保存する
+	const saveTodos = () => {
+		if (!browser) return;
+		localStorage.setItem('todos', JSON.stringify(todos));
+	};
+
+	onMount(() => {
+		todos = getTodos();
+	});
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>TODO</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1>todo list</h1>
+	<input type="text" bind:value={task} />
+	<button on:click={addTask}>追加</button>
+	<ul>
+		{#each todos as todo (todo.id)}
+			<li>
+				{todo.task}
+				<button on:click={() => deleteTask(todo.id)}>Delete</button>
+			</li>
+		{/each}
+	</ul>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
